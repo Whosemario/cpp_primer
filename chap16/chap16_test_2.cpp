9ad667c089e476716f7641ced7433bc27d3fa7b4
whosemario <gx546532416@gmail.com>
2016-11-09 20:48:34 +0800

    关于右值引用模板

--- chap16/chap16_test_2.cpp
+++ chap16/chap16_test_2.cpp
@@ -2,6 +2,8 @@
 	16.2.5 模板实参推断引用
 */
 
+#include <iostream>
+
 // p必须是左值，如果p是常量，T将推断为const
 template<typename T>
 void f1(T& p) {}
@@ -9,6 +11,15 @@ void f1(T& p) {}
 // p可以是左值，也可以是右值；可以是常量，也可以是非常量
 template<typename T>
 void f2(const T& p) {}
+
+// 引用折叠
+// p为右值：T为非引用
+// p为左值：T为引用
+template<typename T>
+void f3(T&& p) {
+	T t = p;
+	t = t + 1;
+}
 
 int main(int args, char* argv[])
 {
@@ -21,6 +32,10 @@ int main(int args, char* argv[])
 	f2(ci);
 	f2(47);
 	f2(i);
+
+	f3(i);   // i -> 48
+	// f3(ci); error: T is const int&
+	std::cout << i << std::endl;
 
 	return 0;
 }
